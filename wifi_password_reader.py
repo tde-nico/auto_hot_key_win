72bda583cfec48d33753f7b46792bce0fbd354c7
tde-nico <tommyden5@gmail.com>
2024-10-12 10:36:59 +0200

    update wifi pass reader

--- wifi_password_reader.py
+++ wifi_password_reader.py
@@ -2,12 +2,12 @@ import subprocess
 
 data = subprocess.check_output(['netsh','wlan','show','profiles'])
 data = data.decode('cp437').split('\n') # cp1252 or utf-8
-profiles = [profile.split(":")[1][1:-1] for profile in data if "Tutti i profili utente" in profile] # all users profiles
+profiles = [profile.split(":")[1][1:-1] for profile in data if "All User Profile" in profile] # all users profiles
 
 for profile in profiles:
 	results = subprocess.check_output(['netsh','wlan','show','profile',profile,'key=clear'])
 	results = results.decode('cp437').split('\n') # cp1252 or utf-8
-	results = [content.split(":")[1][1:-1] for content in results if "Contenuto chiave" in content] # key content
+	results = [content.split(":")[1][1:-1] for content in results if "Key Content" in content] # key content
 	try:
 		print("{:<30}|  {:<}".format(profile, results[0]))
 	except:
